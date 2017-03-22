81ed5f8439bb8f89c670da1a84cbb314109b4207
Evan Short <evan.short@gmail.com>
2017-03-22 13:44:16 -0400

    adding element to represent a stack in a list

---
+++ src/app/stack-list-element/component.stack-list-element.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from 'material-ui/Layout';
+
+const FCD_StackListElement = ( { name } ) => {
+  return (
+    <Layout>
+      { name }
+    </Layout>
+	)
+};
+export default FCD_StackListElement;
+
