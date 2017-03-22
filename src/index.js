79e456c53abdfbf1e501977424b8041162d807b0
Evan Short <evan.short@gmail.com>
2017-03-21 23:18:16 -0400

    adding layout theme

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import 'babel-polyfill'
 
+
 import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
+injectTapEventPlugin();
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+
+// TEMPORARY //
 const flashcardReducer = (state) => state
 const initialState = {}
 
 let store = createStore(flashcardReducer, initialState)
+///////////////
 
 render(
-  <Provider store={store}>
-    <div>hello world!</div>
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <div>hello world!</div>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('fcd-root')
 )
 
